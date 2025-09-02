6b51d7f3af014e87410a9afcd0fdd195c4bbf522
Yash Jadhav <ygjadhav04@gmail.com>
2025-09-02 18:45:48 +0530

    Modify setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 from setuptools import setup, find_packages
+from typing import List
+
+HYPHEN_DOT = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+
+        if HYPHEN_DOT in requirements:
+            requirements.remove(HYPHEN_DOT)
+
+    return requirements
+
 
 setup(
     name='student-performance-ml',
     version='0.1',
     author = 'Yash Jadhav',
     author_email = 'ygjadhav04@gmail.com',
-    packages=find_packages(),
-    install_requires=[
-        'pandas',
-        'scikit-learn',
-        'matplotlib',
-        'seaborn'
-    ]   
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
 )
